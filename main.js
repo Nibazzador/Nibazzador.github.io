e51b9d85ad3ae1eabfd4a26490c6ce3bef8fae3e
NiilaVest <88927707+Nibazzador@users.noreply.github.com>
2023-07-13 12:51:04 +0300

    Section bgs and nav cards on computer

--- main.js
+++ main.js
@@ -9,3 +9,20 @@ if (
 } else {
   document.body.classList.add("light-theme");
 }
+
+//////////////////////////// NAV CARDS Z-INDEX ////////////////////////////
+
+let cards = document.querySelectorAll(".nav-links > a");
+let cardsOrder = Array.from(cards);
+cards.forEach(function (card) {
+  card.addEventListener("mouseover", function () {
+    cardsOrder = cardsOrder.filter((c) => c !== card);
+    cardsOrder.unshift(card);
+    cardsOrder.forEach(
+      (card) =>
+        (cardsOrder[cardsOrder.indexOf(card)].style.zIndex = `${
+          20 - cardsOrder.indexOf(card)
+        }`)
+    );
+  });
+});
