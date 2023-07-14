3aa1e3dfad5fde60537cc5a69018aad2fde0cff5
NiilaVest <88927707+Nibazzador@users.noreply.github.com>
2023-07-14 16:10:20 +0300

    Improved arrow nav

--- main.js
+++ main.js
@@ -10,6 +10,22 @@ if (
   document.body.classList.add("light-theme");
 }
 
+/////////////////////////// HIDE HEROPHOTO IF OVERLAPS TEXT /////////////////////////////////////
+heroImg = document.querySelector(".hero-photo");
+heroPara = document.querySelector(".hero-p");
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth < 1281) {
+    if (window.innerHeight * 0.4 < heroPara.getBoundingClientRect().bottom) {
+      heroImg.style.display = "none";
+    } else {
+      heroImg.style.display = "block";
+    }
+  } else {
+    heroImg.style.display = "block";
+  }
+});
+
 //////////////////////////// NAV CARDS Z-INDEX ////////////////////////////
 
 let cards = document.querySelectorAll(".nav-links > a");
@@ -28,14 +44,8 @@ cards.forEach(function (card) {
 });
 
 /////////////////////////// LINK BUTTON /////////////////////////////////////
-navButton = document.querySelector(".arrow-nav");
+const navButton = document.querySelector(".arrow-nav");
 
-let observers = [];
-let observerOptions = {
-  root: null,
-  rootMargin: "0px",
-  threshold: [1],
-};
 const targets = [
   "#hero",
   "#nav",
@@ -46,52 +56,38 @@ const targets = [
   "#mina",
   "#footer",
 ];
-let callbackCount = 0;
-const changeHref = (ez) => {
-  if (ez !== "footer") {
-    navButton.setAttribute("href", targets[targets.indexOf(`#${ez}`) + 1]);
-    navButton.classList.remove("up");
-  } else {
-    navButton.setAttribute("href", "#hero");
-    navButton.classList.add("up");
-  }
-};
 
-intersectionCallback = (entries) => {
-  entries.forEach((entry) => {
-    changeHref(entry.target.id);
-    callbackCount++;
-    if (callbackCount === 8) {
-      navButton.setAttribute("href", "#nav");
+const sections = document.querySelectorAll(targets);
+
+let activeIndex = 0;
+
+window.addEventListener("scroll", () => {
+  sections.forEach((section) => {
+    if (section.getBoundingClientRect().top <= 0) {
+      activeIndex = targets.indexOf(`#${section.id}`);
     }
   });
-};
-for (let i = 0; i < 8; i++) {
-  observers[i] = new IntersectionObserver(
-    intersectionCallback,
-    observerOptions
-  );
-  observers[i].observe(document.querySelector(`${targets[i]}`));
-}
-navButton.addEventListener("click", () => {
-  if (window.location.href.split("#")[1] === navButton.href.split("#")[1]) {
-    changeHref(navButton.href.split("#")[1]);
+  navButton.href = targets[activeIndex + 1];
+  if (targets[activeIndex + 1] === "#footer") {
+    if (
+      Math.round(sections[7].getBoundingClientRect().bottom) ===
+      window.innerHeight
+    ) {
+      navButton.href = "#hero";
+      navButton.classList.add("up");
+    } else {
+      navButton.classList.remove("up");
+    }
+  } else {
+    navButton.classList.remove("up");
   }
 });
 
-/////////////////////////// HIDE HEROPHOTO IF OVERLAPS TEXT /////////////////////////////////////
-heroImg = document.querySelector(".hero-photo");
-heroPara = document.querySelector(".hero-p");
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth < 1281) {
-    console.log("ASD");
-    if (window.innerHeight * 0.4 < heroPara.getBoundingClientRect().bottom) {
-      heroImg.style.display = "none";
-    } else {
-      heroImg.style.display = "block";
-    }
-  } else {
-    heroImg.style.display = "block";
+navButton.addEventListener("click", () => {
+  const currentSectionIndex = targets.indexOf(
+    navButton.href.slice(navButton.href.lastIndexOf("#"))
+  );
+  if (sections[currentSectionIndex].getBoundingClientRect().top === 16) {
+    navButton.href = `#${sections[currentSectionIndex + 1].id}`;
   }
 });
